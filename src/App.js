4586df6d82607b0763be6ff92be00c6388e11e1a
unknown <soleil-2005@mail.ru>
2021-06-23 11:59:29 +0300

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a Whatsapp</h1>
       <div className="app__body">
-        {/* Sidebar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>

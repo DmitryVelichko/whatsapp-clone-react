88ab8d29feba98d79b13e72d4472935862d5b319
unknown <soleil-2005@mail.ru>
2021-06-24 11:05:01 +0300

    New Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from '@material-ui/icons'
 
 function Sidebar() {
     return (
@@ -11,14 +12,19 @@ function Sidebar() {
             <div className="sidebar__header">
                 <Avatar />
                 <div className="sidebar__headerRight">
+                  <IconButton>
                     <DonutLargeIcon />
+                  </IconButton>
+                  <IconButton>
                     <ChatIcon />
+                  </IconButton>
+                  <IconButton>
                     <MoreVertIcon /> 
-                    
+                  </IconButton>            
                 </div>
             </div>
             <div className="sidebar__search">
-
+                <SearchOutlined />
             </div>   
             <div className="sidebar_chats">
 

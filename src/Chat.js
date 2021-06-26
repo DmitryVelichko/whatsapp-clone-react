c5f85bbb367666aab9a1de3f24efad0db00853a2
unknown <soleil-2005@mail.ru>
2021-06-26 12:22:05 +0300

    New Commit

--- src/Chat.js
+++ src/Chat.js
@@ -3,9 +3,15 @@ import './Chat.css';
 
 function Chat() {
     return (
-        <div className='chat'>
+      <div className='chat'>
+        <div className="chat__header">
+
+        </div>    
+
+        <div className="chat__body">
             
         </div>
+      </div>
     )
 }
 

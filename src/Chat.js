34deb25ea2dd7fb4de851bc2edbf57b78d421602
unknown <soleil-2005@mail.ru>
2021-06-26 12:34:50 +0300

    New Commit

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
 import { Avatar } from '@material-ui/core';
 
 function Chat() {
+    const [seed, setSeed] = useState("");
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random * 5000));
+    }, []);
+
     return (
       <div className='chat'>
         <div className="chat__header">
-            <Avatar />
+            <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
         </div>    
 
         <div className="chat__body">

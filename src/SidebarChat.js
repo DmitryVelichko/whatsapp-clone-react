5ab98a6cd109d04f1d82ee4b6e71ad09fa7e68af
unknown <soleil-2005@mail.ru>
2021-06-26 09:40:32 +0300

    New Commit

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Avatar } from "@material-ui/core";
 import './SidebarChat.css';
 
 
 function SidebarChat() {
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, [])
+
     return (
         <div className='sidebarChat'>
             <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>

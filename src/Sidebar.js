d37ec47ddd72cae2bb9ec92148d839686d2fcd23
unknown <soleil-2005@mail.ru>
2021-06-26 09:57:36 +0300

    New Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -38,7 +38,7 @@ function Sidebar() {
               <SidebarChat />
               <SidebarChat />
               <SidebarChat />
-              <SidebarChat />
+              
             </div>
         </div>
     );
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -3,14 +3,14 @@ import { Avatar } from "@material-ui/core";
 import './SidebarChat.css';
 
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
     const [seed, setSeed] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, [])
 
-    return (
+    return !addNewChat ? (
         <div className='sidebarChat'>
             <Avatar src={`https://avatars.dicebear.com/api/bottts/${seed}.svg`}/>
             <div className="sidebarChat__info">
@@ -18,6 +18,11 @@ function SidebarChat() {
                 <p>Last message...</p>
             </div>
         </div>
+    ): (
+    <div onClick={createChat} className="sidebarChat">
+        <h2>Add new Chat</h2>
+    </div>
+
     )
 }
 

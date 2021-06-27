82035e2ababcb8964a0101e422e8ac1535308fd8
unknown <soleil-2005@mail.ru>
2021-06-27 11:50:34 +0300

    New Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,16 +12,20 @@ function Sidebar() {
     const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
-      db.collection('rooms').onSnapshot(snapshot => (
+      const unsubscribe = db.collection('rooms').onSnapshot((snapshot) => (
         // docs are n documents in the rooms database
-        setRooms(snapshot.docs.map(doc => 
-          ({
+        setRooms(
+            snapshot.docs.map((doc) => ({
             id: doc.id,
             data: doc.data(),
-          })
+          }))
         ))
-      ))
-    }, [])
+      );
+
+      return () => {
+        unsubscribe();
+      }
+    }, []);
 
     return (
         <div className='sidebar'>

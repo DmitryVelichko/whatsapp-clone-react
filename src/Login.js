66011787f7dc0d64d6af83f65a9ef0739f9f6cbe
unknown <soleil-2005@mail.ru>
2021-06-28 18:13:01 +0300

    New Commit

--- src/Login.js
+++ src/Login.js
@@ -3,10 +3,16 @@ import { Button } from '@material-ui/core';
 import './Login.css';
 //WA logo online src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg'
 import logo from './images/WhatsAppLogo.svg';
+import { auth, provider } from './firebase';
 
 function Login() {
 
-    const signIn = () => {};
+    const signIn = () => {
+        auth
+            .signInWithPopup(provider)
+            .then((result) => console.log(result))
+            .catch((error) => alert(error.message));
+    };
 
     return (
         <div className='login'>
